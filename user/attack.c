59de13df27d5a91da1a7068036b03f80f82d2c5c
Jabin <793159320@qq.com>
2024-12-29 12:28:53 +0800

    lab2 system calls

--- user/attack.c
+++ user/attack.c
@@ -6,8 +6,12 @@
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
-
-  exit(1);
+  if (argc != 1) {
+    printf("Usage: attack\n");
+    exit(1);
+  }
+  char *end = sbrk(PGSIZE*32);
+  end += 16 * PGSIZE;
+  write(2, end+32, 8);
+  exit(0);
 }
